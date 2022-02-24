983a2498e1da116df8d44362f67c021004c2195e
Montmo90 <72647113+Montmo90@users.noreply.github.com>
2022-02-24 11:11:22 +0100

    Layout et gestion view

--- src/main/java/fr/eni/tpServlets/servlets/IndexServlet.java
+++ src/main/java/fr/eni/tpServlets/servlets/IndexServlet.java
@@ -1,11 +1,15 @@
 package fr.eni.tpServlets.servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.tpServlets.util.ServletsTools;
 
 /**
  * Servlet implementation class IndexServlet
@@ -27,7 +31,8 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setAttribute("texte", "Lol");
+		ServletsTools.Render(request, response, "test");		
 	}
 
 	/**
---
+++ src/main/java/fr/eni/tpServlets/util/ServletsTools.java
@@ -0,0 +1,23 @@
+package fr.eni.tpServlets.util;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletsTools {
+
+	public static void Render(HttpServletRequest request, HttpServletResponse response, String view) {
+		request.setAttribute("view", "./../view/" + view + ".jsp");
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/layout/default.jsp");
+		try {
+			rd.forward(request, response);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
