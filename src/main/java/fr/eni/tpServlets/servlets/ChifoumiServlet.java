a29ab2a8dac4b96654b0d6e316208bc58442efcc
Killian-Hama <81709882+Killian-Hama@users.noreply.github.com>
2022-02-24 16:42:04 +0100

    Update ChifoumiServlet.java

--- src/main/java/fr/eni/tpServlets/servlets/ChifoumiServlet.java
+++ src/main/java/fr/eni/tpServlets/servlets/ChifoumiServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ChifoumiServlet
  */
-@WebServlet("/ChifoumiServlet")
+@WebServlet("/chifoumi")
 public class ChifoumiServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
