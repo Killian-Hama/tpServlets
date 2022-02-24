46c5d821e5b7765b4b92aa2e1e366fd8939ef981
Killian-Hama <81709882+Killian-Hama@users.noreply.github.com>
2022-02-24 12:23:47 +0100

    SuiviRepas

---
+++ src/main/java/fr/eni/tpServlets/servlets/SuiviRepasHistoriqueServlet.java
@@ -0,0 +1,44 @@
+package fr.eni.tpServlets.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.tpServlets.util.ServletsTools;
+
+/**
+ * Servlet implementation class SuiviRepasServlet
+ */
+@WebServlet("/SuiviRepasServlet")
+public class SuiviRepasHistoriqueServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SuiviRepasHistoriqueServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		ServletsTools.Render(request, response, "suiviRepas");		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
